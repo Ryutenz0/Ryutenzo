0d8c8cde1c522215c5b9dc75d23b4dc86e1815ff
Ryutenzo <ferrykurnia002@gmail.com>
2022-02-23 20:35:09 +0700

    Ryutenzo

---
+++ shop/donasi.js
@@ -0,0 +1,22 @@
+const donasibot = () => {
+return `DONASI BISA VIA PULSA / E-MONEY
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0838 1908 2780
+2. OVO
+	ㅁ 0838 1908 2780
+3. DANA
+	ㅁ 0838 1908 2780
+	
+Semuanya atas nama FE***K*****
+
+*📲 PULSA*
+
+   ㅁ 0838 1908 2780
+`
+	}
+
+exports.donasibot = donasibot
+ 
