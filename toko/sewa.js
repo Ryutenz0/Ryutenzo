6c694587ea363128cd5a15bdbe5a0f85ad255870
Ryutenz0 <97373650+Ryutenz0@users.noreply.github.com>
2022-03-01 07:21:57 +0700

    Update and rename shop/sewa.js to toko/sewa.js

---
+++ toko/sewa.js
@@ -0,0 +1,34 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 10.000
+⬡ 1 BULAN = Rp 15.000
+
+⬣ MINAT? PM
+⬡ wa.me/6283819082780
+
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0838 1908 2780
+2. OVO
+	ㅁ 0838 1908 2780
+3. DANA
+	ㅁ 0838 1908 2780
+	
+Semuanya atas nama FE***K*****
+
+*📲 PULSA*
+
+        ㅁ 0838 1908 2780
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
